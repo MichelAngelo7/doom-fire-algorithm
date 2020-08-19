a1ccf05a203887b8030a018337b7c9c6a9a74225
michelangelo7 <m_che_l@hotmail.com>
2020-08-19 17:02:36 -0300

    Create the data struct and test array

--- fire.js
+++ fire.js
@@ -1,12 +1,19 @@
-const firePixelsArrey = []
+const firePixelsArray = []
 const fireWidth = 2
-const fireWidth = 3
+const fireHeight = 3
 
 function start() {
+    createFireDataStructure()
+    console.log(firePixelsArray)
 
 }
 
 function createFireDataStructure() {
+    const numberOfPixels = fireWidth * fireHeight
+
+    for (let i = 0; i < numberOfPixels; i++) {
+        firePixelsArray[i] = 0
+    }
 
 }
 
